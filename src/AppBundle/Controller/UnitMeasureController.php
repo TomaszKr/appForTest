42d52d0162fea443c9cb3d1f0de74df3193f23ac
Tomasz Krol <tomasz46@gmail.com>
2018-11-30 23:09:25 +0100

    change routing in AppBundle

--- src/AppBundle/Controller/UnitMeasureController.php
+++ src/AppBundle/Controller/UnitMeasureController.php
@@ -4,18 +4,15 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends Controller
+class UnitMeasureController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
+    public function add(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    
 }
